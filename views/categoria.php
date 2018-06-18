7569b7ea79096084efba791a5a8e5b8e302ced53
Alex Chambi <alex.chambi@hotmail.com>
2018-06-18 10:01:58 -0500

    corrigiendo error

--- views/categoria.php
+++ views/categoria.php
@@ -10,11 +10,8 @@ require 'header.php';
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">
-                            Categoría : <button class="btn btn-success" id="btnagregar" onclick="mostrarFormulario(true)"><i class="fa fa-plus-circle"></i> Agregar</button>
-                          </h1>
+                          <h1 class="box-title">Categoría <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
                         <div class="box-tools pull-right">
-                        -
                         </div>
                     </div>
                     <!-- /.box-header -->
@@ -27,7 +24,7 @@ require 'header.php';
                             <th>Descripción</th>
                             <th>Estado</th>
                           </thead>
-                          <tbody>                         
+                          <tbody>                            
                           </tbody>
                           <tfoot>
                             <th>Opciones</th>
@@ -37,31 +34,24 @@ require 'header.php';
                           </tfoot>
                         </table>
                     </div>
-
                     <div class="panel-body" style="height: 400px;" id="formularioregistros">
-                        
                         <form name="formulario" id="formulario" method="POST">
-                        
-                          <div class="form-group col-xs-12 col-sm-6">
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <label>Nombre:</label>
                             <input type="hidden" name="idcategoria" id="idcategoria">
                             <input type="text" class="form-control" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" required>
                           </div>
-                        
-                          <div class="form-group col-xs-12 col-sm-6">
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <label>Descripción:</label>
-                            <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="45" placeholder="Descripción">
+                            <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="256" placeholder="Descripción">
                           </div>
-                        
                           <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <button type="submit" id="btnGuardar" class="btn btn-primary" ><i class="fa fa-save"></i> Guardar</button>
+                            <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
 
-                            <button onclick="cancelarFormulario()" type="button" class="btn btn-danger"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                            <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                           </div>
-                        
                         </form>
                     </div>
-
                     <!--Fin centro -->
                   </div><!-- /.box -->
               </div><!-- /.col -->
--- views/scripts/categoria.js
+++ views/scripts/categoria.js
@@ -1,107 +1,110 @@
 var tabla;
 
+//Función que se ejecuta al inicio
 function init(){
-    mostrarFormulario(false);
-    listar();
+	mostrarform(false);
+	listar();
 
-    $("#formulario").on("submit", function(e){
-        guardaryeditar(e);
-    })
-
+	$("#formulario").on("submit",function(e)
+	{
+		guardaryeditar(e);	
+	})
 }
 
-function limpiar(){
-    $("#nombre").val("");
-    $("#descripcion").val("");
-    $("#idcategoria").val("");
+//Función limpiar
+function limpiar()
+{
+	$("#nombre").val("");
+	$("#descripcion").val("");
+	$("#idcategoria").val("");
 }
 
-function mostrarFormulario(flag){
-    limpiar();
-    if(flag){
-        $("#listadoregistros").hide();
-        $("#formularioregistros").show();
-        $("$btnGuardar").prop("disabled",false);
-        $("$btnagregar").hide();
-    }
-    else{
-        $("#listadoregistros").show();
-        $("#formularioregistros").hide();
-        $("#btnagregar").show();
-    }
+//Función mostrar formulario
+function mostrarform(flag)
+{
+	limpiar();
+	if (flag)
+	{
+		$("#listadoregistros").hide();
+		$("#formularioregistros").show();
+		$("#btnGuardar").prop("disabled",false);
+		$("#btnagregar").hide();
+	}
+	else
+	{
+		$("#listadoregistros").show();
+		$("#formularioregistros").hide();
+		$("#btnagregar").show();
+	}
 }
 
-function cancelarFormulario(){
-    limpiar();
-    mostrarFormulario(false);
+//Función cancelarform
+function cancelarform()
+{
+	limpiar();
+	mostrarform(false);
 }
 
-function listar(){
-    
-    tabla = $('#tbllistado').dataTable(
-        {
-            /**
-             * procesamiento
-             * paginacion y filtrado
-             * elementos de control
-             */
-            "aProcessing": true, 
-            "aServerSide": true,
-            dom: 'Bfrtip',
-            buttons:    [
-                            'copyHtml5',
-                            'excelHtml5',
-                            'csvHtml5',
-                            'pdf'
-                        ],
-                "ajax": {
-    
-                        url: '../ajax/categoria.php?op=listar',
-                        type: "get",
-                        dataType: "json",
-                        error: function(e){
-                            console.log(e.responseText);
-                        }
-                },
-                /**
-                 * paginacion
-                 * orden de datos
-                 */
-                "bDestroy": true,
-                "iDisplayLength": 5,
-                "order": [[0, "asc"]] 
-        }).DataTable();
+//Función Listar
+function listar()
+{
+	tabla=$('#tbllistado').dataTable(
+	{
+		"aProcessing": true,//Activamos el procesamiento del datatables
+	    "aServerSide": true,//Paginación y filtrado realizados por el servidor
+	    dom: 'Bfrtip',//Definimos los elementos del control de tabla
+	    buttons: [		          
+		            'copyHtml5',
+		            'excelHtml5',
+		            'csvHtml5',
+		            'pdf'
+		        ],
+		"ajax":
+				{
+					url: '../ajax/categoria.php?op=listar',
+					type : "get",
+					dataType : "json",						
+					error: function(e){
+						console.log(e.responseText);	
+					}
+				},
+		"bDestroy": true,
+		"iDisplayLength": 5,//Paginación
+	    "order": [[ 0, "desc" ]]//Ordenar (columna,orden)
+	}).DataTable();
 }
+//Función para guardar o editar
 
 function guardaryeditar(e)
 {
-    e.preventDefault();
-    $("#btnGuardar").prop("disabled",true);
-    var formData = new FormData($("#formulario")[0]);
+	e.preventDefault(); //No se activará la acción predeterminada del evento
+	$("#btnGuardar").prop("disabled",true);
+	var formData = new FormData($("#formulario")[0]);
 
-    $.ajax({
-        url: "../ajax/categoria.php?op=guardaryeditar",
-        type: "POST",
-        data: formData,
-        contentType: false,
-        processData: false,
+	$.ajax({
+		url: "../ajax/categoria.php?op=guardaryeditar",
+	    type: "POST",
+	    data: formData,
+	    contentType: false,
+	    processData: false,
 
-        success: function(datos)
-        {
-            alert(datos);
-            mostrarFormulario(false);
-            tabla.ajax.reload();
-        }
-    });
+	    success: function(datos)
+	    {                    
+	          alert(datos);	          
+	          mostrarform(false);
+	          tabla.ajax.reload();
+	    }
 
-    limpiar();
+	});
+	limpiar();
 }
+
 function mostrar(idcategoria)
 {
 	$.post("../ajax/categoria.php?op=mostrar",{idcategoria : idcategoria}, function(data, status)
 	{
 		data = JSON.parse(data);		
-		mostrarFormulario(true);
+		mostrarform(true);
 
 		$("#nombre").val(data.nombre);
 		$("#descripcion").val(data.descripcion);
@@ -109,5 +112,34 @@ function mostrar(idcategoria)
 
  	})
 }
+
+//Función para desactivar registros
+function desactivar(idcategoria)
+{
+	bootbox.confirm("¿Está Seguro de desactivar la Categoría?", function(result){
+		if(result)
+        {
+        	$.post("../ajax/categoria.php?op=desactivar", {idcategoria : idcategoria}, function(e){
+        		alert(e);
+	            tabla.ajax.reload();
+        	});	
+        }
+	})
+}
+
+//Función para activar registros
+function activar(idcategoria)
+{
+	bootbox.confirm("¿Está Seguro de activar la Categoría?", function(result){
+		if(result)
+        {
+        	$.post("../ajax/categoria.php?op=activar", {idcategoria : idcategoria}, function(e){
+        		alert(e);
+	            tabla.ajax.reload();
+        	});	
+        }
+	})
+}
+
 
 init();
